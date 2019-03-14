66ba0aa0b21a829989bcf2b85226a2bd1cb0f910
Taras <php.laboratory@gmail.com>
2019-03-14 14:01:08 +0200

    Add blank contract for Deal

--- contracts/FiduceDeal.sol
+++ contracts/FiduceDeal.sol
@@ -1,7 +1,32 @@
 pragma solidity >=0.4.21 <0.6.0;
 
-contract FiduceDeal {
-    function FiduceDeal(){
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "zos-lib/contracts/Initializable.sol";
+import "./User.sol";
+
+contract FiduceDeal is Ownable,Initializable {
+    User _client;
+    User _agent;
+    User _bank;
+
+    enum  state {Created, Initial }
+
+
+    constructor () public {
+        state = Initial;
+    }
+
+    function initialize(address agent, address client) onlyOwner initializable {
+        _agent = agent;
+        _client = client;
 
     }
+    function setBank(address bank) onlyOwner public {
+        _bank = bank;
+    }
+    function addDocument(address document) onlyOwner public {
+
+    }
+
+
 }
